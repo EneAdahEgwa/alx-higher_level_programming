a071a6f80eb207b51b01b384999e4c130c9463fd
EneAdahEgwa <eneadahegwa@gmail.com>
2023-01-11 03:26:28 -0800

    p

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary) <= 0:
-        return None
-    return max(a_dictionary, key=a_dictionary.get)
+def multiply_by_2(a_dictionary):
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict.update({key: (value * 2)})
+    return new_dict
